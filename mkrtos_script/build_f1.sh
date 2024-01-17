4d89ed81e9faa4149a11846d8c82ae9573ddfdb8
zhangzheng <1358745329@qq.com>
2024-01-18 01:09:39 +0800

    优化stm32f1支持

--- mkrtos_script/build_f1.sh
+++ mkrtos_script/build_f1.sh
@@ -4,13 +4,7 @@
 # export TOOLCHAIN_LIB=/home/ATShining/gcc-arm-none-eabi-5_4-2016q3/lib/gcc/arm-none-eabi/5.4.1/armv7-m
 export TOOLCHAIN=/Users/zhangzheng/gcc-arm-none-eabi-10.3-2021.10/bin/
 export TOOLCHAIN_LIB=/Users/zhangzheng/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/thumb/v7-m/nofp
-export KEN_OFFSET=0x2000
-export INIT_OFFSET=0x10000
-export BOOTFS_ADDR_OFFSET=0x22000
-export KNL_DATA=0x20000000
-export CPU_TYPE=STM32
-export BOARD=STM32F1x
-export ARCH=cortex-m3
+export BOARD=STM32F103ZET6
 
 set -e
 cmake -G Ninja -B build/$KNL .
