f0811b0395d268de47494840a45284ae935fc531
zhangzheng <1358745329@qq.com>
2024-01-21 16:15:58 +0800

    一个精简的printf支持，剪裁后只占用几KB ROM

--- mkrtos_user/lib/sys/inc/u_arch.h
+++ mkrtos_user/lib/sys/inc/u_arch.h
@@ -19,7 +19,8 @@
 
 static inline uint16_t swap_u16(uint16_t data)
 {
-    return ((data >> 8) & 0xff) | ((data & 0xff) << 8);
+    return (uint16_t)((data >> (uint16_t)8) & (uint16_t)0xffUL) |
+           (uint16_t)((uint16_t)(data & (uint16_t)0xff) << (uint16_t)8);
 }
 
 int mk_syscall(unsigned long nr, ...);
--- mkrtos_user/server/hello/src/main.c
+++ mkrtos_user/server/hello/src/main.c
@@ -1,21 +1,23 @@
 
-// #include <stdio.h>
-#include <cons_cli.h>
+#include <printf.h>
+#include <unistd.h>
 int main(int argc, char *args[])
 {
-    cons_write_str("Hello world.\n");
-    // printf("print test0.\n");
-    // printf("print test1.\n");
-    // printf("print test2.\n");
-    // float a = 1.1;
-    // float b = 1.2;
-    // float c;
+    mk_printf("print test0.\n");
+    mk_printf("print test1.\n");
+    mk_printf("print test2.\n");
+    float a = 1.1;
+    float b = 1.2;
+    float c;
 
-    // while (1)
-    // {
-    //     c = a + b;
-    //     printf("%c %d %f\n", 'a', 1234, 1.1);
-    //     printf("%c %d %lf\n", 'a', 1234, a * b);
-    // }
+    while (1)
+    {
+        c = a + b;
+        // mk_printf("%c %d %f\n", 'a', 1234, 1.1);
+        // mk_printf("%c %d %lf\n", 'a', 1234, a * b);
+        mk_printf("%c %d %d\n", 'a', 1234, (int)1.1);
+        mk_printf("%c %d %d\n", 'a', 1234, (int)(a * b));
+        sleep(1);
+    }
     return 0;
 }
